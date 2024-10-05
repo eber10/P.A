d0627fde1880047386ba5810d0167702dd0a9b3a
eber10 <eber.penaloza10@gmail.com>
2024-10-05 14:22:48 -0500

    método de ordenación por inserción directa

---
+++ inserDir.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void inserDir(int arr[], int n)
+{
+    for(int i=2; i<n; i++)
+    {
+        int aux=arr[i];
+        int k=i-1;
+        while(k>0 && aux<arr[k])
+        {
+            arr[k+1]=arr[k];
+            k--;
+        }
+        arr[k+1]=aux;
+    }
+    cout<<"valores ordenador"<<endl;
+    cout<<"------------------"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
+int main()
+{
+    int n;
+    cout<<"ingrese la cantidad de elementos:"; cin>>n;
+    int arreglo[n];
+    for(int i=0; i<n; i++)
+    {
+        cout<<i<<":"; cin>>arreglo[i];
+    }
+    inserDir(arreglo, n);
+    return 0;
+}
